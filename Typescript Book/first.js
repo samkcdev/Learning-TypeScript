476a07ae009ad165abe8f727dedb25ed084d3830
Sam Cherian <samnp87@gmail.com>
2019-05-16 21:22:36 +0530

    Corrected mistakes in exercise

--- Typescript Book/first.js
+++ Typescript Book/first.js
@@ -1,16 +1,15 @@
 (function () {
     var button = document.querySelector(".button");
-    var input = document.querySelector(".form-control");
+    var input = (document.querySelector(".form-control"));
     var errmsg = document.querySelector(".errorMsg");
     var messegner = document.querySelector(".message");
-    button.addEventListener("click", greetButton("Hello", "No Name Entered"));
+    button.addEventListener("click", greetButton("Hello ", "No Name Entered"));
     function greetButton(prefix, errorMsg) {
-        console.log(input.value);
-        messegner.innerHTML = prefix + input.value;
         return function (e) {
             if (input.value.length === 0) {
                 return (errmsg.innerHTML = errorMsg);
             }
+            messegner.innerHTML = prefix + input.value;
         };
     }
 })();
--- Typescript Book/first.ts
+++ Typescript Book/first.ts
@@ -1,18 +1,19 @@
 (function() {
   var button: Element = document.querySelector(".button");
-  var input = <HTMLInputElement>document.querySelector(".form-control");
+  var input: HTMLInputElement = <HTMLInputElement>(
+    document.querySelector(".form-control")
+  );
 
   var errmsg: HTMLElement = document.querySelector(".errorMsg");
   var messegner: HTMLElement = document.querySelector(".message");
-  button.addEventListener("click", greetButton("Hello", "No Name Entered"));
+  button.addEventListener("click", greetButton("Hello ", "No Name Entered"));
 
   function greetButton(prefix: string, errorMsg: string) {
-    console.log(input.value);
-    messegner.innerHTML = prefix + input.value;
     return function(e: MouseEvent) {
       if (input.value.length === 0) {
         return (errmsg.innerHTML = errorMsg);
       }
+      messegner.innerHTML = prefix + input.value;
     };
   }
 })();
